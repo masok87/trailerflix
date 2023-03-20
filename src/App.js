da62e3da5d7527ba22ad25601860dae236b87aef
masok87 <mdanca1987@gmail.com>
2023-03-20 15:23:02 +0000

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
+  const API_URL = "https://api.themoviedb.org/3";
+  const [movies, setMovies] = useState([]);
+  const fetchMovies = async () => {
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/discover/movie`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+      },
+    });
+    setMovies(results);
+  };
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  const renderMovies = () =>
+    movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Heloo Trailerflix</h1>
+      <div className="container">{renderMovies()}</div>
     </div>
   );
 }
