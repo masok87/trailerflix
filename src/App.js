d7af712f79fcd09ad1c246d5fde78cc60d58cd72
cornel97 <cornelilinca80@gmail.com>
2023-03-21 13:49:07 +0000

    search bar

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import MovieCard from "./components/MovieCard";
 function App() {
   const API_URL = "https://api.themoviedb.org/3";
   const [movies, setMovies] = useState([]);
-  const fetchMovies = async () => {
+  const [searchKey, setSearchKey] = useState("");
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
     const {
       data: { results },
-    } = await axios.get(`${API_URL}/discover/movie`, {
+    } = await axios.get(`${API_URL}/${type}/movie`, {
       params: {
         api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        query: searchKey,
       },
     });
     setMovies(results);
@@ -21,9 +24,21 @@ function App() {
   }, []);
   const renderMovies = () =>
     movies.map((movie) => <MovieCard key={movie.id} movie={movie} />);
+
+  const searchMovies = (e) => {
+    e.preventDefault();
+    fetchMovies(searchKey);
+  };
   return (
     <div className="App">
-      <h1>This is our TrailerFlix</h1>
+      <header>
+        <h1>This is our TrailerFlix</h1>
+
+        <form onSubmit={searchMovies}>
+          <input type="text" onChange={(e) => setSearchKey(e.target.value)} />
+          <button type={"submit"}>Search!</button>
+        </form>
+      </header>
       <div className="container">{renderMovies()}</div>
     </div>
   );
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,19 @@
 import React from "react";
+
 const MovieCard = ({ movie }) => {
-  return <div>{movie.title}</div>;
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
+  return (
+    <div className={"movie-card"}>
+      {movie.poster_path ? (
+        <img
+          className={"movie-cover"}
+          src={`${IMAGE_PATH}${movie.poster_path}`}
+          alt=""
+        />
+      ) : null}
+      <h5 className={"movie-title"}>{movie.title}</h5>
+    </div>
+  );
 };
 
 export default MovieCard;
